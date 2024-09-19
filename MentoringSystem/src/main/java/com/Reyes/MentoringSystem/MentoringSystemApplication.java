935514b8e7282bcd83e3bab4237049bb867582f7
BenjaminGRUPO6 <lreyesg7@upao.edu.pe>
2024-09-19 10:22:32 -0500

    estructura basica

---
+++ MentoringSystem/src/main/java/com/Reyes/MentoringSystem/MentoringSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Reyes.MentoringSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentoringSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentoringSystemApplication.class, args);
+	}
+
+}
---
+++ MentoringSystem/src/test/java/com/Reyes/MentoringSystem/MentoringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Reyes.MentoringSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoringSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
